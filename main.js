b65c8a18a7f1bb1ad4fe51ca61a88b1f023f6c68
Kadin Wilson <kadin.t.wilson@gmail.com>
2021-12-29 06:04:50 -0800

    Generate word object from random word in list
    word size and speed are determined by word length

--- main.js
+++ main.js
@@ -1,19 +1,23 @@
 import {Word} from './word.js';
+import {wordList} from './wordList.js';
 
 const canvas = document.getElementById('game');
 const ctx = canvas.getContext('2d');
 canvas.width = window.innerWidth;
 canvas.height = window.innerHeight;
 
+const SIZE_MULTIPLIER = canvas.height / 100; // word size = length * multiplier
+const SPEED_MULTIPLIER = canvas.width / 60; 
+const MAX_SPEED = canvas.width / 5;          // word speed = max - length * multiplier
+
 let words = [];
 let previousTime;
 
-words.push(new Word('example', 25, 50, canvas.width + 10, 30));
-words.push(new Word('example', 25, 50, canvas.width + 10, 60));
-words.push(new Word('example', 25, 50, canvas.width + 10, 90));
-words.push(new Word('example', 25, 50, canvas.width + 10, 120));
-
+let wordInterval = setInterval(() => {
+    words.push(randomWord(wordList, canvas.width + 10, 30, canvas.height - 30));
+}, 1000);
 requestAnimationFrame(loop);
+
 
 function loop(time) {
     if (previousTime === undefined)
@@ -38,3 +42,11 @@ function draw() {
         word.draw(ctx);
 }
 
+function randomWord(list, x, minY, maxY) {
+    // random y between min and max
+    let y = Math.floor((Math.random() * (maxY - minY)) + minY);
+    let word = list[Math.floor(Math.random() * list.length)];
+
+    return new Word(word, word.length * SIZE_MULTIPLIER,
+                    MAX_SPEED - (word.length * SPEED_MULTIPLIER), x, y);
+}
